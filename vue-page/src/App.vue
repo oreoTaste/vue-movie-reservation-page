76ed3f79758b3353be8fa48ef97803265e6ee3a9
Youngkuk Sohn <youngkuk.sohn@gmail.com>
2021-04-26 20:20:51 +0900

    install vue.js

---
+++ vue-page/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <index msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import index from './components/index.vue'
+
+export default {
+  name: 'App',
+  components: {
+    index
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
